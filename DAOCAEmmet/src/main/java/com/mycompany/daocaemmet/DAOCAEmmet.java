f604261e81bd5a2b6ea47df8846739788c153f4a
emmet <emmet@DESKTOP-L59408F>
2025-01-08 20:04:24 +0000

    Test commit2

---
+++ DAOCAEmmet/src/main/java/com/mycompany/daocaemmet/DAOCAEmmet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.daocaemmet;
+
+/**
+ *
+ * @author emmet
+ */
+public class DAOCAEmmet {
+
+    public static void main(String[] args) {
+        System.out.println("test commit!");
+    }
+}
